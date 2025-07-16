639e22a8cc37e88f07e9053ebf328ec82741c99c
Anton Els <anton.els@fivetran.com>
2025-07-16 21:14:43 +1200

    Add extra validation

--- hd-debug-k8s.sh
+++ hd-debug-k8s.sh
@@ -1,132 +1,153 @@
 #!/bin/bash
 #
-# This script provides debugging utilities for the HD agent
-#
-# Usage:
-#   ./hd-debug-k8s.sh [-n namespace] [-h]
-#
-# Options:
-#   -h, --help      Show this help message and exit
-#   -n <namespace>  Specify the Kubernetes namespace to check (defaults to 'default')
+# This is a helper script to collet and log diagnostics for the Hybrid Deployment Agent in a Kubernetes environment.
 #
 # Requirements:
 #   - kubectl must be installed and configured
 #   - The HD agent should be installed and running
 #
-NAMESPACE="default"
+# Usage:
+#   ./hd-debug-k8s.sh [-n namespace] [-h]
+#
+# set -x
+# set -e
 
+# Image for the Hybrid Deployment Agent container
+HD_AGENT_IMAGE="us-docker.pkg.dev/prod-eng-fivetran-ldp/public-docker-us/ldp-agent:production"
 # defined in templates/deployment.yaml
 HD_AGENT_DEPLOYMENT_CONTAINER_NAME="hd-agent"
 # defined in templates/configmap.yaml
 HD_AGENT_CONFIG_NAME="hd-agent-config"
+NAMESPACE="default"
+TEST_POD_NAME="hd-test-$$-pod"
+# For slower networks or clusters, you may want to increase the timeout
+TIMEOUT=60
+
+# Key endpoints to verify connectivity
+ENDPOINTS=(
+    "https://ldp.orchestrator.fivetran.com"
+    "https://api.fivetran.com/v1/hybrid-deployment-agents"
+    "https://us-docker.pkg.dev"
+    "https://storage.googleapis.com/fivetran-metrics-log-sr"
+)
+
+SCRIPT_PATH="$(realpath "$0")"
+BASE_DIR="$(dirname "$SCRIPT_PATH")"
+DIAG_DIR="$BASE_DIR/k8s_stats/"
+rm -r "$DIAG_DIR" 2>/dev/null
+mkdir -p "$DIAG_DIR" 2>/dev/null
+
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M)
+AGENT_DEPLOYMENT=""
+AGENT_POD=""
+
+
+if [ "$UID" -eq 0 ]; then
+    echo -e "This script should not be run as root user.\n"
+    exit 1
+fi
 
 function usage() {
-cat <<EOF
-Usage: $0 [-n <namespace>] [-h]
-  -n  Kubernetes namespace (optional, default: default)
-  -h  Display this help message
-EOF
+    echo -e "Usage: $0 [-n <namespace>] [-h]\n"
     exit 1
+}
+
+function check_kubectl() {
+ if ! command -v kubectl &> /dev/null; then
+    echo "kubectl command line utility not found. Please install the latest version."
+    echo -e "https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/\n"
+    exit 1
+ fi
+}
+
+function check_helm() {
+ if ! command -v helm &> /dev/null; then
+    echo "helm command line utility not found. Please install the latest version."
+    echo -e "https://helm.sh/docs/intro/install/\n"
+    exit 1
+ fi
+}
+
+function get_agent_deployment_name() {
+    AGENT_DEPLOYMENT=$(kubectl get deployments -n "$NAMESPACE" -l app.kubernetes.io/name=hd-agent --no-headers -o custom-columns=":metadata.name")
+    if [ -z "$AGENT_DEPLOYMENT" ]; then
+        echo "No Agent pod found in '$NAMESPACE'"
+    fi
+}
+
+function get_agent_pod_name() {
+    AGENT_POD=$(kubectl get pod -n "$NAMESPACE" -l app.kubernetes.io/name=hd-agent --no-headers -o custom-columns=":metadata.name")
+    if [ -z "$AGENT_POD" ]; then
+        echo "No Agent pod found in '$NAMESPACE'"
+    fi
+}
+
+function log_agent_info() {
+    echo -e "Collecting HD Agent environment diagnostics...\n"
+
+    get_agent_deployment_name
+    get_agent_pod_name
+
+    if [ -z "$AGENT_DEPLOYMENT" ] || [ -z "$AGENT_POD" ]; then
+        echo "No HD Agent deployment or pod found in namespace '$NAMESPACE'."
+        exit 1
+    else
+        echo "Found HD Agent deployment: $AGENT_DEPLOYMENT"
+        echo "Found HD Agent pod: $AGENT_POD"
+
+        echo "This may take a few seconds..."
+    
+        kubectl describe pod "$AGENT_POD" -n "$NAMESPACE" > "$DIAG_DIR/pod_description.log" 2>&1
+        kubectl get events --field-selector involvedObject.name="$AGENT_POD" -n "$NAMESPACE" -o custom-columns=Message:.message --no-headers > "$DIAG_DIR/pod_events.log" 2>&1
+        kubectl top pod "$AGENT_POD" -n "$NAMESPACE" > "$DIAG_DIR/pod_resource_usage.log" 2>&1
+        kubectl get pod "$AGENT_POD" -n "$NAMESPACE" -o yaml > "$DIAG_DIR/pod_definition.log" 2>&1
+        kubectl get deployment $AGENT_DEPLOYMENT -n "$NAMESPACE" -o yaml > agent_deployment.out 2>&1
+        kubectl get jobs "$AGENT_POD" -n "$NAMESPACE" -o yaml > "$DIAG_DIR/jobs.log" 2>&1
+    fi
+
+    kubectl get configmaps -n "$NAMESPACE" > "$DIAG_DIR/configmap_listing.log" 2>&1
+    kubectl get configmap "$HD_AGENT_CONFIG_NAME" -n "$NAMESPACE" -o yaml | grep -v 'token:' > "$DIAG_DIR/agent_config_map.log" 2>&1   
+    kubectl get secrets -n "$NAMESPACE" > "$DIAG_DIR/secrets_listing.log" 2>&1
+
+    local HD_AGENT_NODE_NAME=$(kubectl get pod "$AGENT_POD" -n "$NAMESPACE" -o jsonpath="{.spec.nodeName}")
+    kubectl describe node "$HD_AGENT_NODE_NAME" > "$DIAG_DIR/node_description.log" 2>&1
+
+    kubectl get serviceaccounts -n "$NAMESPACE" > "$DIAG_DIR/service_accounts.log" 2>&1
+    kubectl get roles -n "$NAMESPACE" > "$DIAG_DIR/roles.log" 2>&1
+    kubectl get rolebindings -n "$NAMESPACE" > "$DIAG_DIR/role_bindings.log" 2>&1
+
+    kubectl get pv -n "$NAMESPACE" -o wide > "$DIAG_DIR/pv.log" 2>&1
+    kubectl get pvc -n "$NAMESPACE" -o wide > "$DIAG_DIR/pvc.log" 2>&1
+    kubectl get pvc -n "$NAMESPACE" -o yaml > "$DIAG_DIR/pvc-detail.log" 2>&1
 }
 
 while getopts "n:h" opt; do
     case $opt in
-        n)
-            NAMESPACE="$OPTARG"
-            ;;
-        h)
-            usage
-            ;;
-        \?)
-            echo "ERROR: Invalid option: -$OPTARG" >&2
-            usage
-            ;;
-        :) # Handles missing arguments for options (e.g., -n without a value)
-            echo "ERROR: Option -$OPTARG requires an argument." >&2
-            usage
-            ;;
-        *)
-            echo "ERROR: Unhandled error in argument parsing." >&2
-            usage
-            ;;
+        n) NAMESPACE="$OPTARG" ;;
+        h) usage ;;
+        \?) echo "Invalid option: -$OPTARG" >&2; usage ;;
     esac
 done
-
 shift $((OPTIND-1))
 
-# Check if running as root
-if [ "$UID" -eq 0 ]; then
-    echo -e "This script should not be run as root from the base directory of the Hybrid Deployment Agent.\n Please run as a regular user.\n"
-    exit 1
-fi
-
-
-echo "Namespace: $NAMESPACE"
-
 if [ $# -gt 0 ]; then
-    echo "ERROR: Extra arguments provided: $*"
+    echo "Error: Extra arguments provided: $*"
     usage
     exit 1
 fi
 
-if ! command -v kubectl &> /dev/null; then
-    echo "ERROR: kubectl not found. Please install kubectl."
-    exit 1
-fi
-
-HD_AGENT_POD_NAME=$(kubectl get pods -n "$NAMESPACE" -o jsonpath="{range .items[*]}{.metadata.name}{'\t'}{.spec.containers[*].name}{'\n'}{end}" | grep "$HD_AGENT_DEPLOYMENT_CONTAINER_NAME" | awk '{print $1}' 2>/dev/null)
-if [ -z "$HD_AGENT_POD_NAME" ]; then
-    echo "ERROR: Unable to find HD agent pod in namespace '$NAMESPACE'"
-    exit 1
-fi
-
-SCRIPT_PATH="$(realpath "$0")"
-BASE_DIR="$(dirname "$SCRIPT_PATH")"
-
-DIAGNOSTICS_DIR="$BASE_DIR/k8s_stats/"
-
-rm -r "$DIAGNOSTICS_DIR" 2>/dev/null
-
-mkdir -p "$DIAGNOSTICS_DIR" 2>/dev/null
-if [ ! -d "$DIAGNOSTICS_DIR" ]; then
-    echo "Failed to create diagnostics directory: $DIAGNOSTICS_DIR"
-    exit 1
-fi
-echo -e "Diagnostics location: $DIAGNOSTICS_DIR\n"
-
-TIMESTAMP=$(date +%Y-%m-%d_%H-%M)
-
-ARCHIVE_PATH="$BASE_DIR/k8s_stats/logs-$TIMESTAMP.tar.gz"
-
-function log_agent_info() {
-    echo -e "Collecting HD agent pod stats...\n"
-
-    kubectl describe pod "$HD_AGENT_POD_NAME" -n "$NAMESPACE" > "$DIAGNOSTICS_DIR/pod_description.log" 2>&1
-
-    local HD_AGENT_POD_RELEASE_NAME=$(kubectl get pod "$HD_AGENT_POD_NAME" -n "$NAMESPACE" -o jsonpath="{.spec.containers[?(@.name=='$HD_AGENT_DEPLOYMENT_CONTAINER_NAME')].env[?(@.name=='release_name')].value}")
-
-    kubectl top pod "$HD_AGENT_POD_NAME" -n "$NAMESPACE" > "$DIAGNOSTICS_DIR/pod_resource_usage.log" 2>&1
-
-    kubectl get pod "$HD_AGENT_POD_NAME" -n "$NAMESPACE" -o yaml > "$DIAGNOSTICS_DIR/pod_resource_usage.log" 2>&1
-
-    kubectl get configmaps -n "$NAMESPACE" > "$DIAGNOSTICS_DIR/config_maps.log" 2>&1
-
-    kubectl get configmap "$HD_AGENT_CONFIG_NAME" -n "$NAMESPACE" -o yaml | grep -v 'token:' > "$DIAGNOSTICS_DIR/agent_config_map.log" 2>&1
-    
-    kubectl get secrets -n "$NAMESPACE" > "$DIAGNOSTICS_DIR/secrets.log" 2>&1
-
-    local HD_AGENT_NODE_NAME=$(kubectl get pod "$HD_AGENT_POD_NAME" -n "$NAMESPACE" -o jsonpath="{.spec.nodeName}")
-    kubectl describe node "$HD_AGENT_NODE_NAME" > "$DIAGNOSTICS_DIR/node_description.log" 2>&1
-
-    kubectl get serviceaccounts -n "$NAMESPACE" > "$DIAGNOSTICS_DIR/service_accounts.log" 2>&1
-
-    kubectl get roles -n "$NAMESPACE" > "$DIAGNOSTICS_DIR/roles.log" 2>&1
-
-    kubectl get rolebindings -n "$NAMESPACE" > "$DIAGNOSTICS_DIR/role_bindings.log" 2>&1
-
-    echo "OK"
-}
-
+check_kubectl
+check_helm
 log_agent_info
 
-(cd "$DIAGNOSTICS_DIR" && tar -czf "$ARCHIVE_PATH" *.log)
+echo -e "done.\n"
+echo -e "Packing logs into hd-logs-$TIMESTAMP.tar.gz\n"
+
+cd $DIAG_DIR 
+tar czf hd-logs-$TIMESTAMP.tar.gz ./*.log
+ls -altr hd-logs-$TIMESTAMP.tar.gz
+cd - 
+
+echo -e "done.\n"
+echo -e "Logs are available in $DIAG_DIR/hd-logs-$TIMESTAMP.tar.gz\n"
+
